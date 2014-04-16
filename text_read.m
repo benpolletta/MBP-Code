aff25b79ffeb523e64039fb82ae7d7542e227c5f
benp_mbp <benpolletta@gmail.com>
2014-04-16 17:24:05 -0400

    Script to replace soon to be unsupported textread.

---
+++ text_read.m
@@ -0,0 +1,19 @@
+function [varargout] = text_read(filename,format)
+
+fid = fopen(filename,'r');
+
+text = textscan(fid,format);
+
+fclose(fid);
+
+for n = 1:nargout
+   
+    varargout{n} = text{n};
+    
+end
+
+if nargout~=length(text)
+    
+    display('Unreturned variables have been read.')
+    
+end
