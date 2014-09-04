3ce6e6c77fc2f6fe40aa13e9e06b049908d280ed
benp_mbp <benpolletta@gmail.com>
2014-09-04 15:35:23 -0400

    Script to get (x-axis) position of bars in bar plot.

---
+++ get_bar_pos.m
@@ -0,0 +1,13 @@
+function pos_bars = get_bar_pos(handle)
+
+    for i = 1:length(handle)
+
+        x = get(get(handle(i), 'children'), 'xdata');
+
+        x = mean(x([1 3],:));
+
+        pos_bars(i,:) = x;
+
+    end
+
+end
