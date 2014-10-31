be6978c453ddebf2c9b7d410fce2609b8866b86b
bp_@_scc2 <benpolletta@gmail.com>
2014-10-31 15:37:21 -0400

    Added script to perform wavelet spectrogram, with flipped ends.

--- save_as_pdf.m
+++ save_as_pdf.m
@@ -1,5 +1,5 @@
 function save_as_pdf(handle,title)
 
-saveas(handle,[title,'.fig'])
-set(handle,'PaperOrientation','landscape','PaperUnits','normalized','PaperPosition',[0 0 1 1])
-print(handle,'-dpdf',[title,'.pdf'])
+saveas(handle, [title,'.fig'])
+set(handle, 'PaperOrientation', 'landscape', 'PaperUnits', 'normalized', 'PaperPosition', [0 0 1 1])
+print(handle, '-dpdf', [title,'.pdf'])
---
+++ wavelet_spectrogram.m
@@ -0,0 +1,28 @@
+function ws = wavelet_spectrogram(data, sampling_freq, freqs, no_cycles)
+
+[data_length, no_channels] = size(data);
+
+if isempty(freqs), freqs = 1:(sampling_freq/2); end
+no_freqs = length(freqs);
+
+if isempty(no_cycles), no_cycles = 7*ones(no_freqs,1); end
+    
+wavelets = dftfilt3(freqs, no_cycles, sampling_freq, 'winsize', sampling_freq);
+
+ws = nan(data_length, no_freqs, no_channels);
+
+flip_length = min(sampling_freq, data_length);
+
+data_reflected = [flipud(data(1:flip_length, :)); data; flipud(data((end - flip_length + 1):end, :))];
+
+for ch = 1:no_channels
+    
+    for f = 1:no_freqs
+        
+        conv_prod = conv(data_reflected(:, ch), wavelets(f,:), 'same');
+        
+        ws(:, f, ch) = conv_prod((flip_length + 1):(end - flip_length));
+        
+    end
+    
+end
