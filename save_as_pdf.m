6f8900066ddbe86d530f516bcfa16912dc7b71a5
benp <benpolletta@gmail.com>
2015-10-08 17:43:51 -0400

    Changing resolution in save_as_pdf (probably useless).

--- save_as_pdf.m
+++ save_as_pdf.m
@@ -6,4 +6,4 @@ saveas(handle, [title,'.fig'])
 % set(textobj, 'FontSize', 8)
 
 set(handle, 'PaperOrientation', 'landscape', 'PaperUnits', 'normalized', 'PaperPosition', [0 0 1 1])
-print(handle, '-dpdf', [title,'.pdf'])
+print(handle, '-painters', '-dpdf', '-r600', [title,'.pdf'])
