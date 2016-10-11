0d8686d6f2dc0a5c4a108947f2b0650b31828382
benpolletta <benpolletta@gmail.com>
2016-10-11 15:01:16 -0400

    Scripts for calculating power given p-value.

---
+++ power_2_sample_t_1_sided.m
@@ -0,0 +1,7 @@
+function beta = power_2_sample_t_1_sided(mu1, mu2, sd, n1, n2, alpha)
+
+z = (mu1 - mu2)/(sd.*(sqrt(1./n1 + 1./n2)));
+
+z_alpha = norminv(1 - alpha, 0, 1);
+
+beta = 1 - normpdf(z - z_alpha, 0, 1);
