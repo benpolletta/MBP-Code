a96e46c9e6c86b4391311a45c08ba6bfbec122f1
benpolletta <benpolletta@gmail.com>
2016-10-27 13:47:18 -0400

    Debugging script to compare channels.

--- Bernat_Code/compare_channels.m
+++ Bernat_Code/compare_channels.m
@@ -20,7 +20,7 @@ for d = 1:no_drugs
             
             labels{x_index} = sprintf('%s %s (ch. %d)', subjects{sx}, channel_names{chx}, x_chan);
             
-            x_epoch = load(sprintf('%s_%s/%s_%s_chan%d_epoch%d', subjects{sx}, drug,...
+            x_epoch = load(sprintf('%s_%s/%s_%s_chan%d_epoch%d.txt', subjects{sx}, drug,...
                 subjects{sx}, drug, x_chan, 1123));
             
             for sy = sx:no_subjects
@@ -31,7 +31,7 @@ for d = 1:no_drugs
                     
                     y_index = (sy - 1)*no_channels + chy;
                     
-                    y_epoch = load(sprintf('%s_%s/%s_%s_chan%d_epoch%d', subjects{sy}, drug,...
+                    y_epoch = load(sprintf('%s_%s/%s_%s_chan%d_epoch%d.txt', subjects{sy}, drug,...
                         subjects{sy}, drug, y_chan, 1123));
                     
                     comp_mat(x_index, y_index, d) = sum(x_epoch - y_epoch);
