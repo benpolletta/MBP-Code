aa1baee2bd86b70d84c4fa4cec61a21e82b478a4
benpolletta <benpolletta@gmail.com>
2016-10-27 14:05:47 -0400

    Debugging compare_channels.

--- Bernat_Code/compare_channels.m
+++ Bernat_Code/compare_channels.m
@@ -1,8 +1,8 @@
-function comp_mat = compare_channels
+function [x_nans, y_nans, comp_mat] = compare_channels
 
 load('channels'), load('subjects'), load('drugs')
         
-comp_mat = nan(no_subjects*no_channels, no_subjects*no_channels, no_drugs);
+[x_nans, y_nans, comp_mat] = nan(no_subjects*no_channels, no_subjects*no_channels, no_drugs);
 
 figure
 
@@ -20,9 +20,6 @@ for d = 1:no_drugs
             
             labels{x_index} = sprintf('%s %s (ch. %d)', subjects{sx}, channel_names{chx}, x_chan);
             
-            x_epoch = load(sprintf('%s_%s/%s_%s_chan%d_epoch%d.txt', subjects{sx}, drug,...
-                subjects{sx}, drug, x_chan, 1123));
-            
             for sy = sx:no_subjects
                 
                 for chy = chx:no_channels
@@ -31,10 +28,21 @@ for d = 1:no_drugs
                     
                     y_index = (sy - 1)*no_channels + chy;
                     
-                    y_epoch = load(sprintf('%s_%s/%s_%s_chan%d_epoch%d.txt', subjects{sy}, drug,...
-                        subjects{sy}, drug, y_chan, 1123));
-                    
-                    comp_mat(x_index, y_index, d) = nansum(x_epoch - y_epoch);
+                    for epoch = 1123:1223
+                        
+                        x_epoch = load(sprintf('%s_%s/%s_%s_chan%d_epoch%d.txt', subjects{sx}, drug,...
+                            subjects{sx}, drug, x_chan, epoch));
+                        
+                        x_nans(x_index, y_index, d) = x_nans(x_index, y_index, d) + (sum(~isnan(x_epoch)) == 0);
+                        
+                        y_epoch = load(sprintf('%s_%s/%s_%s_chan%d_epoch%d.txt', subjects{sy}, drug,...
+                            subjects{sy}, drug, y_chan, epoch));
+                        
+                        y_nans(x_index, y_index, d) = y_nans(x_index, y_index, d) + (sum(~isnan(y_epoch)) == 0);
+                        
+                        comp_mat(x_index, y_index, d) = comp_mat(x_index, y_index, d) + nansum(x_epoch - y_epoch);
+                        
+                    end
                     
                 end
                 
@@ -43,6 +51,8 @@ for d = 1:no_drugs
         end
         
     end
+    
+    comp_mat(x_nans(:, :, d) == 0 | y_nans(:, :, d) == 0, d) = max(comp_mat(:, :, d));
     
     figure
     
