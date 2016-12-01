f829f46aa37871437b5797f0af6f8655df19f11f
benpolletta <benpolletta@gmail.com>
2016-12-01 17:40:23 -0500

    Debugging script for swapping occipital data for A103 & A104 NVP and saline.

--- Bernat_Code/saline_NVP_chan3_swap_A103_A104_BP_spec.m
+++ Bernat_Code/saline_NVP_chan3_swap_A103_A104_BP_spec.m
@@ -9,6 +9,8 @@ subj_pairs = nchoosek(1:subj_num, 2); no_subj_pairs = size(subj_pairs, 1);
 drug_labels = {'saline','NVP'};
 drug_num = length(drug_labels);
 
+measures = {'BP', 'spec'};
+
 for d = 1:drug_num
     
     drug = char(drug_labels{d});
@@ -21,15 +23,19 @@ for d = 1:drug_num
         
         to_subj = subject_labels{subj_pairs(p, 1)};
         
-        old_filename = sprintf('ALL_%s_chan%d/ALL_%s_%s_chan%d_BP', from_subj, channel, from_subj, drug, channel);
-        
-        new_filename = sprintf('ALL_%s_chan%d/ALL_%s_%s_chan%d_BP', to_subj, channel, to_subj, drug, channel);
-        
-        load([old_filename, '.mat']);
-        
-        save([old_filename, '_OLD_', datestr(now, 'mm-dd-yy_HH-MM-SS'), '.mat'], 'BP_all', 'band_limits', 'band_labels')
-        
-        save([new_filename, '_NEW_', datestr(now, 'mm-dd-yy-HH-MM-SS'), '.mat'], 'BP_all', 'band_limits', 'band_labels')
+        for m = 1:no_measures
+            
+            old_filename = sprintf('ALL_%s_chan%d/ALL_%s_%s_chan%d_%s', from_subj, channel, from_subj, drug, channel, measures{m});
+            
+            new_filename = sprintf('ALL_%s_chan%d/ALL_%s_%s_chan%d_%s', to_subj, channel, to_subj, drug, channel, measures{m});
+            
+            load([old_filename, '.mat']);
+            
+            save([old_filename, '_OLD_', datestr(now, 'mm-dd-yy_HH-MM-SS'), '.mat'], 'BP_all', 'band_limits', 'band_labels')
+            
+            save([new_filename, '_NEW_', datestr(now, 'mm-dd-yy-HH-MM-SS'), '.mat'], 'BP_all', 'band_limits', 'band_labels')
+            
+        end
         
     end
     
