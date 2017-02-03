dc39d9c42ea1e308c24edafac4b416b909461809
benpolletta <benpolletta@gmail.com>
2017-02-03 17:34:34 -0500

    Script to initialize a structure with a cell of keys, a cell of default values, and key-value pairs to replace the defaults.

---
+++ init_struct.m
@@ -0,0 +1,47 @@
+function is = init_struct(keys, defaults, varargin)
+
+% init_struct is a function that takes in key-value pairs, a list of keys,
+% and a list of corresponding defaults, and returns a struct containing
+% fields named by the keys.
+
+if length(defaults) ~= length(keys)
+    
+    display(sprintf(['The first two arguments of init_struct must be matched 1-D cell arrays\n',...
+        'containing keys and their corresponding default values.']))
+    
+end
+
+is = struct();
+
+for k = 1:length(keys)
+    
+    is.(keys{k}) = defaults{k};
+    
+end
+
+if nargin > 2
+    
+    if mod(nargin, 2)
+        
+        display(sprintf(['The first two arguments of init_struct must be matched 1-D cell arrays\n',...
+            'containing keys and their corresponding default values.',...
+            'Following the first two arguments, the arguments of init_struct must be\n',...
+            'key-value pairs giving non-default parameter values.']));
+        
+    end
+    
+    for a = 1:(floor(nargin/2) - 1)
+        
+        for k = 1:length(keys)
+            
+            if strcmp(varargin{2*a - 1}, keys{k})
+                
+                is.(keys{k}) = varargin{2*a};
+                
+            end
+            
+        end
+        
+    end
+    
+end
