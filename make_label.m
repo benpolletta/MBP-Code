52260a04d234e34adfb04d8ff840c8f92f102dae
benp <benpolletta@gmail.com>
2017-12-19 16:06:57 -0500

    make_label: adding location option, refactoring code.

--- make_label.m
+++ make_label.m
@@ -1,47 +1,60 @@
-function label = make_label(stem, values, defaults)
+function label = make_label(stem, values, defaults, location_option)
+
+if nargin < 4, location_option = []; legend
+if isempty(location_option), location_option = 'front'; end
 
 if nargin < 3, defaults = []; end
 
 if ndims(values) == ndims(defaults)
-    
+
     if any(size(values) ~= size(defaults))
-        
+
         default_flag = false;
-        
+
     else
-        
+
         if all_dimensions(@sum, values ~= defaults) == 0
-            
+
             default_flag = true;
-            
+
         else
-            
+
             default_flag = false;
-            
+
         end
-        
+
     end
 
 else
-    
+
     default_flag = false;
-    
+
 end
 
-if default_flag
-    
-    label = '';
-    
-else
-    
-    label = ['_', stem];
-    
+label = '';
+
+if ~default_flag
+
     for v = 1:length(values)
-        
+
         label = [label, '_', num2str(values(v))];
-        
+
     end
-    
+
+    if ~isempty(stem)
+
+        if strcmp(location_option, 'front')
+
+            label = ['_', stem, label];
+
+        elseif strcmp(location_option, 'back')
+
+            label = [label, '_', stem];
+
+        end
+
+    end
+
 end
 
 end
