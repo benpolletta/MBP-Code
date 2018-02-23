d17a52493b2217f72dc023d6d340b88eeb48c89d
benp <benpolletta@gmail.com>
2018-02-23 13:31:43 -0500

    save_as_eps: commenting out resizing.

--- save_as_eps.m
+++ save_as_eps.m
@@ -5,5 +5,5 @@ saveas(handle, [title,'.fig'])
 % textobj = findobj('Type', 'text');
 % set(textobj, 'FontSize', 8)
 
-set(handle, 'PaperOrientation', 'landscape', 'PaperUnits', 'normalized', 'PaperPosition', [0 0 1 1])
+% set(handle, 'PaperOrientation', 'landscape', 'PaperUnits', 'normalized', 'PaperPosition', [0 0 1 1])
 print(handle, '-depsc', [title,'.eps'])
